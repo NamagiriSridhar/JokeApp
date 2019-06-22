d4a488d85f53da5e0a1d0b785a23eecc9797c142
NamagiriSridhar <49659868+NamagiriSridhar@users.noreply.github.com>
2019-06-22 06:45:17 -0500

    Add files via upload

---
+++ JokeApp/src/main/java/com/tts/JokeApp/services/JokeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.tts.JokeApp.services;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
+public class JokeServiceImpl implements JokeService 
+{
+	
+	private final ChuckNorrisQuotes chuckNorrisQuotes;
+	
+	public JokeServiceImpl()
+	{
+		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+	}
+
+	public String getJoke() 
+	{
+		return chuckNorrisQuotes.getRandomQuote();
+	}
+
+}
